8fcf836ef3ea67e4eb147d7437d086a2adb2f210
Ani-official <aniketkirtane633@gmail.com>
2024-12-14 15:26:33 +0530

    comm

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import { assets } from '../assets/assets'
 import { Link } from 'react-router-dom'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 import { UserContext } from '@clerk/shared/react/index'
+import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
 
   const { openSignIn } = useClerk()
-  const { isSignedIn, user} = useUser( )
+  const { isSignedIn, user} = useUser()
+  const {credit, loadCreditsData} = useContext(AppContext)
+
+  useEffect(()=>{
+    if (isSignedIn) {
+      loadCreditsData()
+    }
+  },[isSignedIn])
+
   return (
     <div className='flex items-center justify-between mx-4 py-3 lg:mx-44'>
       <Link to='/'><img className='w-32 sm:w-44' src={assets.logo} alt="" /></Link>
