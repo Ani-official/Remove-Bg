f2a2c4bbbe7f522fa3ba229526a12db0a3f5345b
Ani-official <aniketkirtane633@gmail.com>
2024-12-14 16:24:58 +0530

    Attaching the clerkId to the request body

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -2,25 +2,28 @@ import jwt from 'jsonwebtoken'
 
 // Middleware Function to decode jwt token to get clerkId
 const authUser = async (req, res, next) => {
-    
     try {
-        
-        const {token} = req.headers
+        const { token } = req.headers;
 
         if (!token) {
-            res.json({success:false, message:"Not Authorized Login Again"})
-        } 
+            return res.status(401).json({ success: false, message: "Not Authorized. Login Again" });
+        }
 
-        const token_decode = jwt.decode(token)
-        req.body.clerkId = jwt.decode.clerkId
-        next()
+        // Decode the token
+        const token_decode = jwt.decode(token);
 
+        if (!token_decode || !token_decode.clerkId) {
+            return res.status(401).json({ success: false, message: "Invalid token or clerkId missing" });
+        }
 
+        // Attach the clerkId to the request body
+        req.body.clerkId = token_decode.clerkId;
+
+        next();
     } catch (error) {
         console.log(error.message);
-        res.json({success:false,message:error.message})
+        return res.status(500).json({ success: false, message: error.message });
     }
-
 }
 
-export default authUser
+export default authUser;
